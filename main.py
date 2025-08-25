c676b14226ff242a097048062c4571d7ed555904
anden1305 <andreasbigom@gmail.com>
2025-08-25 11:00:15 +0200

    added main

--- main.py
+++ main.py
@@ -2,42 +2,11 @@
 
 
 
-from scr.preprocessing.collapse_dimensions import CollapseDimensions
-
-
 if __name__ == "__main__":
     
-    from scr.data.synthetic_loader import SyntheticLoader
-    from scr.data.synthetic_dataset import SyntheticDataset
-    from scr.preprocessing.fft import FFT
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import torch
-    
-    dataset = SyntheticDataset('data/synthetic_data/test_hmm')
-    # Keep a handle to the transform to access window_size for plotting
-    fft_transform = FFT({'window_size': 512})
-    dimension_transform = CollapseDimensions({})
-    dataloader = SyntheticLoader(
-        dataset=dataset,
-        batch_size=5120,
-        shuffle=False,
-        transforms=[
-            fft_transform,
-            dimension_transform
-        ]
-    )
+    from scr.orchestrator.orchestrator import Orchestrator
 
-    data = [(x,y) for x,y in dataloader]
-    x = np.concatenate([xx for xx, _ in data])
-    y = np.concatenate([yy for _, yy in data])
+    config_path = "scr/config/config_no_features.yaml"
 
-    x = torch.from_numpy(x)
-    y = torch.from_numpy(y)
-    
-    print(dataset.data.shape)
-    print(dataset.labels.shape)
-    print(dataset.raw_labels.shape)
-
-    print(x.shape)
-    print(y.shape)
+    orchestrator = Orchestrator(config_path)
+    orchestrator.run()
