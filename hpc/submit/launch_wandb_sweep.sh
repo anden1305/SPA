bb0e78e846f828bdf69715e5daa53bded34b3e3e
Oliver Elmgreen <ollie.elmgreen1@gmail.com>
2025-11-06 15:57:50 +0100

    Increase validation frequency to 100 epochs across various experiment configurations for HMM and MARHMM.

--- hpc/submit/launch_wandb_sweep.sh
+++ hpc/submit/launch_wandb_sweep.sh
@@ -13,7 +13,7 @@ NUM_AGENTS=$2
 ### default values for optional arguments. Default is 1 trial per agent
 TRIALS_PER_AGENT=${3:-1}
 ### default is 4 hours per job
-WALLTIME=${4:-6:00}
+WALLTIME=${4:-3:00}
 ### default is gpuv100, but options are [gpuv100, gpua100, gpua10, gpul40s]
 ### bqueues | grep -i gpu
 QUEUE=${5:-gpul40s}
@@ -86,13 +86,21 @@ echo "Submitted array job. Monitor with bjobs and check hpc/output/sweep_${SWEEP
 ### Example usage:
 
 ### Synthetic
-### bash hpc/submit/launch_wandb_sweep.sh src/config/sweep/hmm/hmm_synth_features.yaml 15 1
-### bash hpc/submit/launch_wandb_sweep.sh src/config/sweep/marhmm/marhmm_synth_features.yaml 15 1
-### bash hpc/submit/launch_wandb_sweep.sh src/config/sweep/hmm/hmm_synth_raw.yaml 15 1
-### bash hpc/submit/launch_wandb_sweep.sh src/config/sweep/marhmm/marhmm_synth_raw.yaml 15 1
+###    features
+### bash hpc/submit/launch_wandb_sweep.sh src/config/sweep/hmm/hmm_synth_features.yaml 12 1
+### bash hpc/submit/launch_wandb_sweep.sh src/config/sweep/marhmm/marhmm_synth_features.yaml 12 1 03:00 gpuv100
+
+###      raw
+### bash hpc/submit/launch_wandb_sweep.sh src/config/sweep/hmm/hmm_synth_raw.yaml 12 1
+### bash hpc/submit/launch_wandb_sweep.sh src/config/sweep/marhmm/marhmm_synth_raw.yaml 12 1
+
 
 ### MSSV
-### bash hpc/submit/launch_wandb_sweep.sh src/config/sweep/hmm/hmm_mssv_features.yaml 15 1
-### bash hpc/submit/launch_wandb_sweep.sh src/config/sweep/marhmm/marhmm_mssv_features.yaml 15 1
+###    features
+### bash hpc/submit/launch_wandb_sweep.sh src/config/sweep/hmm/hmm_mssv_features.yaml 12 1
+### bash hpc/submit/launch_wandb_sweep.sh src/config/sweep/marhmm/marhmm_mssv_features.yaml 12 1
 
+###    raw
+### bash hpc/submit/launch_wandb_sweep.sh src/config/sweep/hmm/hmm_mssv_raw.yaml 12 1
+### bash hpc/submit/launch_wandb_sweep.sh src/config/sweep/marhmm/marhmm_mssv_raw.yaml 12 1
 
