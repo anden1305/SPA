4a3b36dc472bd1640ac4889f3724fcfe7a298a7b
Oliver Elmgreen <ollie.elmgreen1@gmail.com>
2025-11-17 16:19:11 +0100

    update RAW yaml experiemts and more yaml and submit scripts

--- hpc/submit/run_training_raw_mssv.sh
+++ hpc/submit/run_training_raw_mssv.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#BSUB -J no_lr_mssv_features                    
+#BSUB -J raw_mssv_hmm          
 #BSUB -q gpuv100            
 #BSUB -W 08:00  
 #BSUB -n 4
 #BSUB -R "span[hosts=1]"                      
 #BSUB -R "rusage[mem=4GB]"
-#BSUB -o hpc/output/no_lr_mssv_features%J.out          
-#BSUB -e hpc/output/no_lr_mssv_features%J.err     
+#BSUB -o hpc/output/raw_mssv_hmm%J.out          
+#BSUB -e hpc/output/raw_mssv_hmm%J.err     
 #BSUB -gpu "num=1:mode=exclusive_process"  
 
 # module purge
 module load cuda/12.8.1
 source .venv/bin/activate
 mkdir -p hpc/output
-python3 main.py --config src/config/run/hmm/tests/no_lr_mssv_features.yaml
+python3 main.py --config src/config/run/hmm/experiments/experiment_mssv_raw.yaml
